3da8cc03f366b3b12169bdf5a6c61227a89005c6
Michael Chan <mchan10@stuy.edu>
2019-01-31 23:26:15 -0500

    added makeAllSums" "

--- recursion.java
+++ recursion.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class recursion{
   public static double sqrt(double n, double tolerance){
     return sqrth(n, tolerance, n / 2);
@@ -20,6 +21,19 @@ public class recursion{
     }
     else{
       return fibh(n - 1, newnum, temp);
+    }
+  }
+  public static ArrayList<Integer> makeAllSums(int n){
+    ArrayList<Integer> newary = new ArrayList<>();
+    sumsh(n, 0, newary);
+  }
+  private static void sumsh(int n, int count, ArrayList<Integer> ary){
+    if (n == 0){
+      ary.add(count);
+    }
+    else{
+      sumsh(n - 1, count + n, ary);
+      sumsh(n - 1, count, ary);
     }
   }
 }
